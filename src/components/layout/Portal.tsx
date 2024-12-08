449942ae30107e15389d26ff475f53c05099d49b
DTieman <djurre.tieman@gmail.com>
2024-12-08 14:26:36 +0100

    Added optional onclick method for portal

--- src/components/layout/Portal.tsx
+++ src/components/layout/Portal.tsx
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom'
 interface Props {
 	trigger: JSX.Element
 	children: any
+	onClick?: () => void
 }
 
-export const Portal: FunctionComponent<Props> = ({ trigger, children }) => {
+export const Portal: FunctionComponent<Props> = ({ trigger, children, onClick }) => {
 	const [show, setShow] = useState(false)
 
 	const updatedTrigger = React.cloneElement(trigger, {
 		onMouseEnter: () => setShow(true),
 		onMouseLeave: () => setShow(false),
+		onClick: onClick,
 	})
 
 	return (
